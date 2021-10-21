bf367ad1fb3ecdafb6f9c5ace3b9eb3c240d42ab
lfmacahe <lfmacahe@hotmail.com>
2021-10-21 20:41:55 -0300

    iniciando trabalho

---
+++ Cliente.py
@@ -0,0 +1,8 @@
+# Criar Cadastro de Cliente
+
+class Cliente:
+    def __init__(self, nome, end, tel, cep):
+        self.nome = nome
+        self.end = end
+        self.tel = tel
+        self.cep = cep
