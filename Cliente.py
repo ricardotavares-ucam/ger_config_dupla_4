2fb5f8cbfdde87250892a2aae56f8948bb92a00b
lfmacahe <lfmacahe@hotmail.com>
2021-10-21 20:51:40 -0300

    Começando Classe Cliente

--- Cliente.py
+++ Cliente.py
@@ -2,7 +2,18 @@
 
 class Cliente:
     def __init__(self, nome, end, tel, cep):
-        self.nome = nome
-        self.end = end
-        self.tel = tel
-        self.cep = cep
+        self._nome = nome
+        self._end = end
+        self._tel = tel
+        self._cep = cep
+
+        def get_nome(self):
+            return self._nome
+        def get_end(sefl):
+            return self._end
+        def get_end(self):
+            return self._end
+        def get_tel(self):
+            return self._tel
+        def get_cep(self):
+            return self._cep
