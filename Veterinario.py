10c7c1af11a86b131b6694f40d2c86b5088319cc
rafaeld108 <38574440+rafaeld108@users.noreply.github.com>
2021-10-27 17:57:15 -0300

    Criado a classe Veterinario.

---
+++ Veterinario.py
@@ -0,0 +1,21 @@
+class Veterinario:
+    def __init__(self, nome_vet, end_vet, tel_vet):
+        self.nome_vet = nome_vet
+        self.end_vet = end_vet
+        self.tel_vet = tel_vet
+
+        def get_nome_vet(self):
+            return self.nome_vet
+        def get_end_vet(self):
+            return self.end_vet
+        def get_tel_vet(self):
+            return self.tel_vet    
+        
+        def set_nome_vet(self, nome_vet):
+            self.nome_vet = nome_vet
+        
+        def set_end_vet(self, end_vet):
+            self.end_vet = end_vet
+        
+        def set_tel_vet(self, tel_vet):
+            self.tel_vet = tel_vet
