925c5a86cd374473d7284c5500ffad0b6063a54b
rafaeld108 <38574440+rafaeld108@users.noreply.github.com>
2021-10-27 17:40:20 -0300

    Criado a classe Exame.

---
+++ Exame.py
@@ -0,0 +1,10 @@
+class Exame:
+    def __init__(self, des_exame):
+        self.des_exame = des_exame
+
+        def get_des_exame(self):
+            return self.des_exame
+        
+        def set_des_exame(self, des_exame):
+            self.des_exame = des_exame
+        
